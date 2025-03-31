800be40f812121cffe727d51ffc6d16906649527
rosbe6 <rosbinruanop@gmail.com>
2025-03-31 14:34:55 -0600

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://rosbe6.github.io',
+  base: 'porfolio_abed',
+});
